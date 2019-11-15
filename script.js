2686791ad925037215d8ecc27358037972dd8df4
Bugamashoo <bugamashoo@gmail.com>
2019-11-15 15:41:43 -0500

    Initial commit

--- script.js
+++ script.js
@@ -2,26 +2,22 @@ var tPoints = 0; //storage for total points - should start at zero
 var cPoints = 0; //points for correct
 var iPoints = -250; //points for incorrect
 var ssound = 0;
-var rSound = 0;
-var soundImport = 0;
-var soundPicker = 0;
-var soundTemp = 0;
 var i1 = new sound("i1.mp3");
-    var i2 = new sound("i2.mp3");
-    var i3 = new sound("i3.mp3");
-    var i4 = new sound("i4.mp3");
-    var i5 = new sound("i5.mp3");
-    var i6 = new sound("i6.mp3");
-    var i7 = new sound("i7.mp3");
-    var i8 = new sound("i8.mp3");
-    var i9 = new sound("i9.mp3");
-    var c1 = new sound("c1.mp3");
-    var c2 = new sound("c2.mp3");
-    var c3 = new sound("c3.mp3");
-    var c4 = new sound("c4.mp3");
-    var c5 = new sound("c5.mp3");
-    var c6 = new sound("c6.mp3");
-    var c7 = new sound("c7.mp3");
+var i2 = new sound("i2.mp3");
+var i3 = new sound("i3.mp3");
+var i4 = new sound("i4.mp3");
+var i5 = new sound("i5.mp3");
+var i6 = new sound("i6.mp3");
+var i7 = new sound("i7.mp3");
+var i8 = new sound("i8.mp3");
+var i9 = new sound("i9.mp3");
+var c1 = new sound("c1.mp3");
+var c2 = new sound("c2.mp3");
+var c3 = new sound("c3.mp3");
+var c4 = new sound("c4.mp3");
+var c5 = new sound("c5.mp3");
+var c6 = new sound("c6.mp3");
+var c7 = new sound("c7.mp3");
 
 var googleSheetLink = "1NVjO-sR4fXAIAsNsjt5FzexCc0eMbvMmKqnarKYAt54";
 function sound(src) {
@@ -39,7 +35,7 @@ function sound(src) {
   };
 }
 function setup() {
-  loadSounds();
+    $("pts") .html("0")
   myMusic = new sound("0.mp3");
   trivia.loadGoogleSheet(googleSheetLink).then(displayWelcome);
   $(".submitt").on("click", function(e) {
@@ -49,9 +45,8 @@ function setup() {
       method: "GET",
       dataType: "json",
       data: $form.serialize()
-                       })
-      // do something
-      
+    });
+    // do something
   });
 }
 
@@ -63,7 +58,7 @@ function draw() {
   else if (trivia.state == "incorrect") background("red");
   else if (trivia.state == "thankyou") background("purple");
 }
- 
+
 function displayWelcome() {
   $(".screen").hide();
   $("#welcome-screen").show();
@@ -71,11 +66,12 @@ function displayWelcome() {
 
 function displayQuestion() {
   $(".screen").hide();
-  $("#question-screen").show();
   $("#correctAnswer").removeClass("highlight");
+  $("#question-screen").show();
   $("#feedback").hide();
   trivia.insertQuestionInfo();
   trivia.shuffleAnswers();
+    $(pts)
 }
 
 function displayThankyou() {
@@ -91,17 +87,20 @@ function onClickedAnswer(isCorrect) {
     $("#feedback")
       .html("+" + (cPoints * 50 + 100) + " points!")
       .show();
-    cPoints = cPoints++;
+      $("#pts") .html("  |  " + tPoints + " points");
+    cPoints++;
   } else {
-    (tPoints = iPoints + tPoints), (cPoints = 0);
+    tPoints = iPoints + tPoints;
+      cPoints = 0;
     iSound();
     $("#feedback")
       .html(iPoints + " points! Streak reset!")
       .show();
+      $("#pts") .html(tPoints + " points");
   }
-    
+
   $("#correctAnswer").addClass("highlight"); //highlight right answer
-  setTimeout(trivia.gotoNextQuestion, 1500); //wait 1 sec...next question
+  setTimeout(trivia.gotoNextQuestion, 1750); //wait 1 sec...next question
 }
 
 function onClickedStart() {
@@ -116,46 +115,47 @@ var $form = $("form#test-form"),
     "https://script.google.com/macros/s/AKfycbzvwiUH1wQsqqJiLDFwRmdXysiPFVnMBEaPxgtJVNSiXE_L0qPo/exec";
 function iSound() {
   ssound = Math.ceil(Math.random() * 9);
-//  rSound = String("i" + ssound);
-//  soundTemp = rSound;
-//  toString(soundTemp).play();
-    if (ssound == 1) {i1.play();}
-else if (ssound == 2) {i2.play();}
-else if (ssound == 3) {i3.play();}
-else if (ssound == 4) {i4.play();}
-else if (ssound == 5) {i5.play();}
-else if (ssound == 6) {i6.play();}
-else if (ssound == 7) {i7.play();}
-else if (ssound == 8) {i8.play();}
-else if (ssound == 9) {i9.play();}
+  //  rSound = String("i" + ssound);
+  //  soundTemp = rSound;
+  //  toString(soundTemp).play();
+  if (ssound == 1) {
+    i1.play();
+  } else if (ssound == 2) {
+    i2.play();
+  } else if (ssound == 3) {
+    i3.play();
+  } else if (ssound == 4) {
+    i4.play();
+  } else if (ssound == 5) {
+    i5.play();
+  } else if (ssound == 6) {
+    i6.play();
+  } else if (ssound == 7) {
+    i7.play();
+  } else if (ssound == 8) {
+    i8.play();
+  } else if (ssound == 9) {
+    i9.play();
+  }
 }
 function cSound() {
   ssound = Math.ceil(Math.random() * 7);
-//  rSound = String("c" + ssound);
-//  soundTemp = rSound;
-//  toString(soundTemp).play();
-    if (ssound == 1) {c1.play();}
-else if (ssound == 2) {c2.play();}
-else if (ssound == 3) {c3.play();}
-else if (ssound == 4) {c4.play();}
-else if (ssound == 5) {c5.play();}
-else if (ssound == 6) {c6.play();}
-else if (ssound == 7) {c7.play();}
+  //  rSound = String("c" + ssound);
+  //  soundTemp = rSound;
+  //  toString(soundTemp).play();
+  if (ssound == 1) {
+    c1.play();
+  } else if (ssound == 2) {
+    c2.play();
+  } else if (ssound == 3) {
+    c3.play();
+  } else if (ssound == 4) {
+    c4.play();
+  } else if (ssound == 5) {
+    c5.play();
+  } else if (ssound == 6) {
+    c6.play();
+  } else if (ssound == 7) {
+    c7.play();
+  }
 }
-function loadSounds() {
-//  soundImport = 0;
-    
-}
-//  var i;
-//  for (i = 0; i < 7; i++) {
-//    soundImport++;
-//    soundPicker = "c" + soundImport + ".mp3";
-//    soundTemp = new sound(String(soundPicker));
-//  }
-//  soundImport = 0;
-//  for (i = 0; i < 9; i++) {
-//    soundImport++;
-//    soundPicker = "i" + soundImport + ".mp3";
-//    soundTemp = new sound(String(soundPicker));
-//  }
-
