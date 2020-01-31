e8454d15d82f3a69648c76b08b6a648378a0f8d1
Bugamashoo <bugamashoo@gmail.com>
2020-01-31 13:25:30 -0500

    misc updates

--- VGFINAL/code.js
+++ VGFINAL/code.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
-    create: create,
-    update: update,
-    render: render
+	create: create,
+	update: update,
+	render: render
 });
 
 var car = [golfCart, truck, apc, foodCart, atv, tank, nascar]; //Load all the cars
@@ -22,84 +22,84 @@ var driveJoints = [];
 
 function create() {
 
-    game.world.setBounds(-10000, -10000, 20000, 20000);
+	game.world.setBounds(-10000, -10000, 20000, 20000);
 
-    game.stage.backgroundColor = '#203050';
+	game.stage.backgroundColor = '#203050';
 
-    // Enable Box2D physics
-    game.physics.startSystem(Phaser.Physics.BOX2D);
-    game.physics.box2d.gravity.y = 280;
-    game.physics.box2d.friction = 0.8;
+	// Enable Box2D physics
+	game.physics.startSystem(Phaser.Physics.BOX2D);
+	game.physics.box2d.gravity.y = 280;
+	game.physics.box2d.friction = 0.3;
 
-    // Make the ground body
-    var groundBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0);
-    groundBody.setChain(groundVertices[sSelection].data);
-
-
-    var jointAnchor = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0); //car limit physics
-    jointAnchor.setPolygon[10, 10, 10, -10, -10, -10, -10, 10];
-
-    var PTM = 50 * cCar.carPTM;
-
-    // Make the car body
-    vehicleVertices = cCar.carVertices;
-    vehicleBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, -40);
-    vehicleBody.setPolygon(vehicleVertices);
-    // bodyA, bodyB, maxForce, maxTorque, correctionFactor, offsetX, offsetY, offsetAngle
-    flipr = game.physics.box2d.motorJoint(jointAnchor, vehicleBody, 0.005, 0, 0);
-    // Make the wheel bodies
+	// Make the ground body
+	var groundBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0);
+	groundBody.setChain(groundVertices[sSelection].data);
 
 
+	var jointAnchor = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0); //car limit physics
+	jointAnchor.setPolygon[10, 10, 10, -10, -10, -10, -10, 10];
 
-    var wheelBodies = [];
-    for (var i = 0; i < cCar.carNumWheels; i++) {
-        wheelBodies[i] = new Phaser.Physics.Box2D.Body(this.game, null, cCarWheel[i].xPos, cCarWheel[i].height, 2);
-        wheelBodies[i].setCircle(cCarWheel[i].size);
-        wheelBodies[i].b2Body.setMassData(cCarWheel[i].grip);
+	var PTM = 50 * cCar.carPTM;
 
-    }
+	// Make the car body
+	vehicleVertices = cCar.carVertices;
+	vehicleBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, -40);
+	vehicleBody.setPolygon(vehicleVertices);
+	// bodyA, bodyB, maxForce, maxTorque, correctionFactor, offsetX, offsetY, offsetAngle
+	flipr = game.physics.box2d.motorJoint(jointAnchor, vehicleBody, 0.005, 0, 0);
+	// Make the wheel bodies
 
 
-    var motorTorque = cCar.carPower;
 
-    // Make wheel joints
-    // bodyA, bodyB, ax, ay, bx, by, axisX, axisY, frequency, damping, motorSpeed, motorTorque, motorEnabled	
-    for (var i = 0; i < cCar.carNumWheels; i++) {
-        driveJoints[i] = game.physics.box2d.wheelJoint(vehicleBody, wheelBodies[i], cCarWheel[i].xPos * 10, cCarWheel[i].height * 10, 0, 0, 0, 1, cCarWheel[i].springs, cCarWheel[i].damping, 0, motorTorque * cCarWheel[i].active, true);
-    }
+	var wheelBodies = [];
+	for (var i = 0; i < cCar.carNumWheels; i++) {
+		wheelBodies[i] = new Phaser.Physics.Box2D.Body(this.game, null, cCarWheel[i].xPos, cCarWheel[i].height, 2);
+		wheelBodies[i].setCircle(cCarWheel[i].size);
+		wheelBodies[i].friction = cCarWheel[i].grip;
+		wheelBodies[i].mass = cCarWheel[i].mass;
+	}
 
-    cursors = game.input.keyboard.createCursorKeys();
 
-    game.camera.follow(vehicleBody);
-    var caption = game.add.text(5, 5, 'Left/right arrow keys to move, down arrow key to brake.', {
-        fill: '#ffffff',
-        font: '14pt Arial'
-    });
-    caption.fixedToCamera = true;
+	var motorTorque = cCar.carPower;
+
+	// Make wheel joints
+	// bodyA, bodyB, ax, ay, bx, by, axisX, axisY, frequency, damping, motorSpeed, motorTorque, motorEnabled	
+	for (var i = 0; i < cCar.carNumWheels; i++) {
+		driveJoints[i] = game.physics.box2d.wheelJoint(vehicleBody, wheelBodies[i], cCarWheel[i].xPos * 10, cCarWheel[i].height * 10, 0, 0, 0, 1, cCarWheel[i].springs, cCarWheel[i].damping, 0, motorTorque * cCarWheel[i].active, true);
+	}
+
+	cursors = game.input.keyboard.createCursorKeys();
+
+	game.camera.follow(vehicleBody);
+	var caption = game.add.text(5, 5, 'Left/right arrow keys to move, down arrow key to brake.', {
+		fill: '#ffffff',
+		font: '14pt Arial'
+	});
+	caption.fixedToCamera = true;
 }
 
 function update() {
 
-    var motorSpeed = 50; // rad/s
-    var motorEnabled = true;
+	var motorSpeed = 50; // rad/s
+	var motorEnabled = true;
 
-    if (cursors.down.isDown) {
-        motorSpeed = 0;
-    } // prioritize braking
-    else if (cursors.left.isDown && !cursors.right.isDown) {
-        motorSpeed *= -1;
-    } else if (cursors.right.isDown && !cursors.left.isDown) {} else {
-        motorEnabled = false;
-    } // roll if no keys pressed
+	if (cursors.down.isDown) {
+		motorSpeed = 0;
+	} // prioritize braking
+	else if (cursors.left.isDown && !cursors.right.isDown) {
+		motorSpeed *= -1;
+	} else if (cursors.right.isDown && !cursors.left.isDown) {} else {
+		motorEnabled = false;
+	} // roll if no keys pressed
 
-    for (var i = 0; i < cCar.carNumWheels; i++) {
-        driveJoints[i].EnableMotor(motorEnabled);
-        driveJoints[i].SetMotorSpeed(motorSpeed);
-    }
+	for (var i = 0; i < cCar.carNumWheels; i++) {
+		driveJoints[i].EnableMotor(motorEnabled);
+		driveJoints[i].SetMotorSpeed(motorSpeed);
+	}
 }
 
 function render() {
 
-    game.debug.box2dWorld();
+	game.debug.box2dWorld();
 
 }
