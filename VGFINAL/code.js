64a7e5cbf59a1afa2ebf0cd42c62ddc1d471de8e
Bugamashoo <bugamashoo@gmail.com>
2020-02-09 22:28:27 -0500

    misc updates

--- VGFINAL/code.js
+++ VGFINAL/code.js
@@ -4,9 +4,9 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
 	render: render
 });
 
-var car = [golfCart, truck, apc, foodCart, atv, tank, nascar, hyperBike, monsterTruck, threeWheeler, dumpTruck, jeep]; //Load all the cars
-//var selection = 0; //CURRENTLY SELECTED CAR (PRESET)
-var selection = Math.floor(Math.random() * 12); //CURRENTLY SELECTED CAR (RANDOM)
+var car = [golfCart, truck, apc, foodCart, atv, tank, nascar, hyperBike, monsterTruck, threeWheeler, dumpTruck, jeep, snowmobile]; //Load all the cars
+//var selection = 12; //CURRENTLY SELECTED CAR (PRESET)
+var selection = Math.floor(Math.random() * 13); //CURRENTLY SELECTED CAR (RANDOM)
 
 var vehicleVertices = [];
 //Car selection
@@ -14,7 +14,10 @@ var Phaser;
 var cursors;
 var vehicleBody;
 var com;
+var comCam;
 var flipr2;
+var bodySprite = car[selection];
+var cameraScale = 2;
 
 var sSelection = 0; //Stage selection
 var groundVertices;
@@ -53,6 +56,7 @@ function refresh() {
 	game.physics.startSystem(Phaser.Physics.BOX2D);
 	game.physics.box2d.gravity.y = 320;
 	game.physics.box2d.friction = 0.25;
+	game.physics.box2d.restitution = 0;
 
 	cursors = game.input.keyboard.createCursorKeys();
 	var groundBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0);
@@ -78,6 +82,8 @@ function refresh() {
 	com.mass = (cCar.carMass * 0.6);
 	game.physics.box2d.weldJoint(vehicleBody, com, cCarX + cCar.carMassX, cCarY + cCar.carMassY)
 	console.log(com);
+	//comCam = new Phaser.Physics.Box2D.Body(this.game, null, cameraScale * 400, cameraScale * -300)
+	//game.physics.box2d.weldJoint(vehicleBody, comCam, (cameraScale * 400), (cameraScale * -300))
 
 	//data.center.set(10, 10);
 	//vehicleBody.setMassData(data);
@@ -113,16 +119,27 @@ function refresh() {
 		partBodies[i2] = new Phaser.Physics.Box2D.Body(this.game, null, cCarPart[i2].xPos, cCarPart[i2].yPos);
 		partBodies[i2].setPolygon(cCarPart[i2].vertices);
 		partBodies[i2].mass = cCarPart[i2].mass;
+		partBodies[i2].friction = cCarPart[i2].grip;
 	}
 	for (var i5 = 0; i5 < cCar.carNumParts; i5++) {
 		// bodyA, bodyB, ax, ay, bx, by, motorSpeed, motorTorque, motorEnabled, lowerLimit, upperLimit, limitEnabled
-		partJoints[i5] = game.physics.box2d.revoluteJoint(vehicleBody, partBodies[i5], cCarPart[i5].xPos + cCarPart[i5].xOff, cCarPart[i5].yPos + cCarPart[i5].yOff, cCarPart[i5].xOff, cCarPart[i5].yOff, null, null, false, cCarPart[i5].limCounterclockwise, cCarPart[i5].limClockwise, true);
-		// bodyA, bodyB, maxForce, maxTorque, correctionFactor, offsetX, offsetY, offsetAngle
-		partJoints2[i5] = game.physics.box2d.motorJoint(vehicleBody, partBodies[i5], null, cCarPart[i5].spring, cCarPart[i5].torque, 0, cCarPart[i5].correctionAngle, cCarPart[i5].correctionAngle)
-
+		if (cCarPart[i5].partType == 0) {
+			partJoints[i5] = game.physics.box2d.revoluteJoint(vehicleBody, partBodies[i5], cCarPart[i5].xPos + cCarPart[i5].xOff, cCarPart[i5].yPos + cCarPart[i5].yOff, cCarPart[i5].xOff, cCarPart[i5].yOff, null, null, false, cCarPart[i5].limCounterclockwise, cCarPart[i5].limClockwise, true);
+			partJoints2[i5] = game.physics.box2d.motorJoint(vehicleBody, partBodies[i5], null, cCarPart[i5].spring, cCarPart[i5].torque, 0, cCarPart[i5].correctionAngle, cCarPart[i5].correctionAngle);
+		} else
+			// bodyA, bodyB, ax, ay, bx, by, motorSpeed, motorTorque, motorEnabled, lowerLimit, upperLimit, limitEnabled
+			if (cCarPart[i5].partType == 2) {
+				partJoints2[i5] = game.physics.box2d.motorJoint(vehicleBody, partBodies[i5], cCarPart[i5].xPos, cCarPart[i5].yPos, cCarPart[i5].xOff, cCarPart[i5].yOff, cCarPart[i5].spring, cCarPart[i5].torque)
+			} else
+				// bodyA, bodyB, maxForce, maxTorque, correctionFactor, offsetX, offsetY, offsetAngle
+				if (cCarPart[i5].partType == 1) {
+					partJoints2[i5] = game.physics.box2d.weldJoint(vehicleBody, partBodies[i5], cCarPart[i5].xPos + cCarPart[i5].xOff, cCarPart[i5].yPos + cCarPart[i5].yOff, cCarPart[i5].xOff, cCarPart[i5].yOff, cCarPart[i5].spring, cCarPart[i5].torque)
+				}
 	}
-	game.camera.follow(com);
 
+	game.camera.follow(com);
+	console.log(comCam);
+	console.log(game.camera);
 }
 
 function update() {
